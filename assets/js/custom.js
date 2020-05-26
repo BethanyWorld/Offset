e0add1a3da764f3586a9000ab102bd5b789c1a09
mona <@gmail.commahsanikbakht>
2020-05-27 01:36:17 +0430

    user comment & menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,7 @@
         var one = $(".one-slider");     
         var two = $(".two-slider");
         var three = $(".three-slider");
+        var four = $(".four-slider");
         one.owlCarousel({
             margin: 0,
             dots: true,
@@ -64,6 +65,22 @@
                 },
                 1000: {
                     items: 3
+                }
+            }
+        });
+        four.owlCarousel({
+            loop: true,
+            margin: 20,
+            // nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
                 }
             }
         });
